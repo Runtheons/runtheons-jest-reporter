4fcfb975752ecdc5d18e0710a7ee691d49760a52
Zexal0807 <gallinar00@gmail.com>
2022-03-02 11:20:29 +0100

    Implement send notify config

--- Reporter.js
+++ Reporter.js
@@ -5,25 +5,34 @@ class Reporter {
 	_globalConfig = null;
 	_options = null;
 
-	constructor(globalConfig, { output, outputType, outputFile, nodemailer }) {
+	constructor(
+		globalConfig, { output, outputType, outputFile, notify, nodemailer, receiver }
+	) {
 		this._globalConfig = globalConfig;
-
-		if (
-			nodemailer == undefined ||
-			nodemailer.host == undefined ||
-			nodemailer.auth == undefined ||
-			nodemailer.auth.user == undefined ||
-			nodemailer.auth.pass == undefined
-		) {
-			throw "ERROR Jest Reporter - nodemailer config not set";
-		}
 
 		this._options = {
 			output: output || true,
 			outputType: outputType.toLowerCase() || "json",
 			outputFile: outputFile || "./status.json",
-			nodemailer: nodemailer,
+			notify: notify || false,
+			nodemailer: nodemailer || {},
+			receiver: receiver || undefined,
 		};
+
+		if (notify) {
+			if (
+				this._options.nodemailer == undefined ||
+				this._options.nodemailer.host == undefined ||
+				this._options.nodemailer.auth == undefined ||
+				this._options.nodemailer.auth.user == undefined ||
+				this._options.nodemailer.auth.pass == undefined
+			) {
+				throw "ERROR Jest Reporter - nodemailer config not set";
+			}
+			if (this._options.receiver == undefined) {
+				throw "ERROR Jest Reporter - notify receiver not set";
+			}
+		}
 	}
 
 	async onRunComplete(contexts, results) {
@@ -41,8 +50,10 @@ class Reporter {
 					break;
 			}
 		}
-		if (results.numFailedTests > 0) {
-			await this.sendNotify();
+		if (this._options.notify) {
+			if (results.numFailedTests > 0) {
+				await this.sendNotify();
+			}
 		}
 	}
 
@@ -75,8 +86,7 @@ class Reporter {
 
 		// send mail with defined transport object
 		let info = await transporter.sendMail({
-			from: "",
-			to: "",
+			to: this._options.receiver,
 			subject: "Autotester failed",
 			text: "Autotester failed",
 		});
